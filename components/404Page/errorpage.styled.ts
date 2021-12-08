a40ffa17672f2c35285e9476ccea6207715a8c94
Alejandro Yunes <alejandro.yunes@inxeption.com>
2021-12-07 23:03:25 -0500

    adding 404

---
+++ components/404Page/errorpage.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  padding: 80px 24px;
+  height: 60vh;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
